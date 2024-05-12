838dba191c38f91f8a3bc4a77ea83d9f5bcab8e4
YashVishal <yashvishalbhat@gmail.com>
2024-05-12 21:48:27 +0530

    Added Test 2  till Place Order

--- cypress/integration/examples/Test2.js
+++ cypress/integration/examples/Test2.js
@@ -1,1 +1,33 @@
-console.log("Hello")
+//cypress - Spec
+/// <reference types="Cypress" />
+
+describe('Second Test Suite', function () {
+    it('Second Test Case', function () {
+
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+        cy.get('input[placeholder="Search for Vegetables and Fruits"]').type('ca')
+        cy.wait(2000)
+
+        cy.get('.products').as('productLocator')
+
+        //Each Loop - Kind of acts as a for loop 
+        cy.get('@productLocator').find('.product').each(($el, index, $list) => {
+            const veggieText = $el.find('h4.product-name').text()
+            if (veggieText.includes('Cashew')) {
+                cy.wrap($el).find('button').click()
+                console.log(veggieText)
+            }
+
+        }
+        )
+
+        cy.get('.cart-icon > img').click()
+        cy.contains('PROCEED TO CHECKOUT').click()
+        cy.contains('Place Order').click()
+
+
+    }
+    )
+
+}
+)
