87b6b6d5cad2707f145eef953ae5d746c8d9bab6
YashVishal <59933537+YashVishal@users.noreply.github.com>
2024-05-19 19:32:37 +0530

    StaticDropdown

--- cypress/integration/examples/WebUI.js
+++ cypress/integration/examples/WebUI.js
@@ -1,4 +1,5 @@
 
+
 /// <reference types = "Cypress" />
 
 describe('WebTest Suite', function () {
@@ -6,12 +7,16 @@ describe('WebTest Suite', function () {
 
 
         cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+        
+        //Checkbox
         cy.get('input#checkBoxOption1').check().should('be.checked').and('have.value','option1')
         cy.get('input#checkBoxOption1').uncheck().should('not.be.checked')
 
         //Selecting Multiple Checkboxes at once
         cy.get('input[type="checkbox"]').check(['option2','option3']).should('be.checked')
 
+        //Static Dropdown - Select method takes in value or Text visible on Ui attribute 
+        cy.get('select#dropdown-class-example').select('option2').should('have.value','option2')
 
 
     }
